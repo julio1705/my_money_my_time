2a5808e25050dd5fb63c07198d73021de82f30dc
Júlio Calheiros <t-julio.calheiros@cni.com.br>
2024-09-13 17:40:18 -0300

    creation of the transitions route

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 
+import transitionsRoutes from './routes/transitions';
+
 const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello API' });
-});
+app.use('/', transitionsRoutes)
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
