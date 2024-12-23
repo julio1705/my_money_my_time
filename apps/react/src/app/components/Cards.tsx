4524bb34fa01d52338f427693065499f8125e026
Júlio Calheiros <t-julio.calheiros@cni.com.br>
2024-12-23 13:38:19 -0300

    fix remaining color card

--- apps/react/src/app/components/Cards.tsx
+++ apps/react/src/app/components/Cards.tsx
@@ -1,25 +1,27 @@
 import './Cards.css'
-
 import { DashboardProps } from '../types/dashboard'
-
 const Cards = ({ incomes, expenses, remaining }: DashboardProps) => {
-
+    const remainingColor = (remaining: string) => {
+        const splitRemaining = remaining.split('')
+        const isNegative = splitRemaining[0] === '-'
+        if (isNegative) return 'red'
+        return 'green'
+    }
     return (
         <div className="container-cards">
             <div className="cards-control">
                 <div className="card-title">Entrada:</div>
-                <div className="card-value" id="budgets">R$ {incomes}</div>
+                <div className="card-value" id="green">R$ {incomes}</div>
             </div>
             <div className="cards-control">
                 <div className="card-title">Saída:</div>
-                <div className="card-value" id="expenses">R$ {expenses}</div>
+                <div className="card-value" id="red">R$ {expenses}</div>
             </div>
             <div className="cards-control">
                 <div className="card-title">Saldo:</div>
-                <div className="card-value" id="incomes">R$ {remaining}</div>
+                <div className="card-value" id={remainingColor(remaining)}>R$ {remaining}</div>
             </div>
         </div>
     )
 }
-
 export default Cards
